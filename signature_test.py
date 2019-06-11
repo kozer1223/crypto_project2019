380e063b871a6112d8fedfa13ebc89d9be1db32a
kozer1223 <kozerskilo@interia.pl>
2019-06-11 23:03:18 +0200

    Modified signature test to show serialization

--- signature_test.py
+++ signature_test.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-from modules.signature import sign, verify_sign
+from modules.signature import sign, verify_sign, write_signature, read_signature
 from modules.sparsegraph import SparseGraph
 from modules.hamiltoniankey import HamiltonianKey
 from modules.genkey import generate_key
+
 
 n = 64
 key = generate_key(n)
 m = 'hello kolokwium'
 
-signature = sign(m, key)
+json_sign = write_signature(sign(m, key))
+signature = read_signature(json_sign)
 
 print(verify_sign(m, key, signature))
 print(verify_sign(m + '!', key, signature))
